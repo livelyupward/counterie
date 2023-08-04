0ac8f29c1cc697c9d0a8228cc16befe7e415db6f
Dave Grammas <dave@Daves-MacBook-Pro-2.local>
2023-08-03 19:58:40 -0500

    initial commit

---
+++ CounterieApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterieApp.swift
+//  Counterie
+//
+//  Created by Dave Grammas on 8/3/23.
+//
+
+import SwiftUI
+
+@main
+struct CounterieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
